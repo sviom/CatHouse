50760afe504cdfcdd729b7f200793b07cbb59754
sviom <kanghanstar@gmail.com>
2015-10-30 01:07:03 +0900

    DB 관련 내용 수정 - Insert 추가

--- CatHouse_Renewal/CatHouse_Renewal/Controllers/RegistController.cs
+++ CatHouse_Renewal/CatHouse_Renewal/Controllers/RegistController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CatHouse_Renewal.Models;
+using CatHouse_Renewal.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,11 @@ namespace CatHouse_Renewal.Controllers
 {
     public class RegistController : Controller
     {
+        // Insert 관련 함수들 모음
+        InsertDB insertDB = new InsertDB();
+        // 점검 관련 함수들 모음
+        Check check = new Check();
+
         // GET: Regist
         //public ActionResult Index()
         //{
@@ -39,7 +46,48 @@ namespace CatHouse_Renewal.Controllers
         public void MemeberCreate()
         {
             // 비밀번호 암호화
+            try
+            {
+                string memName = Request.Form["memName"];
+                string memEmail = Request.Form["memEmail"];
+                string memPassword = Request.Form["memPW"];
+                int memPhone = Convert.ToInt32(Request.Form["memPhone"]);
+                string memAddress = Request.Form["memAddress"];
 
+                // 실제로 체크
+                bool emailCheck = check.CheckEmail(memEmail);
+                bool passCheck = check.CheckPasswordLength(memPassword);
+
+                // 이메일형식 또는 패스워드 길이가 맞지 않으면
+                if (!emailCheck || !passCheck)
+                {
+                    throw new Exception();
+                }
+
+                // 새로운 멤버 객체 생성
+                MemberModel newMem = new MemberModel();
+                newMem.memName = memName;
+                newMem.memEmail = memEmail;
+                newMem.memPassword = memPassword;
+                newMem.memPhone = memPhone;
+                newMem.memAddress = memAddress;
+
+                insertDB.MemberInsert(newMem);
+                // 아이템 추가
+                // 결과에 따라서 환영페이지로 이동할지, 오류 메시지 출력 후 홈페이지로 이동할 지 결정
+                //if (queryResult)
+                //{
+                //    //return RedirectToAction("MemberWelcome", "Member");
+                //}
+                //else
+                //{
+                //    throw new Exception();
+                //}
+            }
+            catch
+            {
+                //return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
--- CatHouse_Renewal/CatHouse_Renewal/DB/InsertDB.cs
+++ CatHouse_Renewal/CatHouse_Renewal/DB/InsertDB.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using CatHouse_Renewal.Models;
 
 namespace CatHouse_Renewal.DB
 {
     public class InsertDB
     {
+        // DBConnection 객체를 이용해 DB를 열고 닫는다.
+        DBConnection db = new DBConnection();
+        // DBConnection 객체에서 받아온 SqlConnection 객체를 받아옴.
+        private static SqlConnection conn;
+
+        public void MemberInsert(MemberModel memItem)
+        {
+            try
+            {
+                // DB를 새로 연다.
+                conn = db.DbOpen();
+                //열려 있으면 사용한다.
+                if (conn.State.ToString() != "Open")
+                {
+                    conn.Open();
+                }
+                else
+                {
+                    // 고유아이디/이름/나이/성별/중성화상태/사진/상태메모
+                    string query = "INSERT INTO dbo.Member (memName,memPassword,memAddress,memEmail,memPhone) VALUES (@memName,@memPassword,@memAddress,@memEmail,@memPhone)";
+                    SqlCommand sqlQuery = new SqlCommand(query, conn);
+                    sqlQuery.Parameters.AddWithValue("@memName", memItem.memName);
+                    sqlQuery.Parameters.AddWithValue("@memPassword", memItem.memPassword);
+                    sqlQuery.Parameters.AddWithValue("@memAddress", memItem.memAddress);
+                    sqlQuery.Parameters.AddWithValue("@memEmail", memItem.memEmail);
+                    sqlQuery.Parameters.AddWithValue("@memPhone", memItem.memPhone);
+                    // 실행
+                    int rows = sqlQuery.ExecuteNonQuery();
+                }
+                db.DbClose();
+                //return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                sqlEx.Message.ToString();
+                //return false;
+            }
+        }
     }
 }
