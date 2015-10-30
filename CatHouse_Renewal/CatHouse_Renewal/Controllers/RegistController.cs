1fa05c064d72e0a2da9d72fa7e7de8fa870d0c37
sviom <kanghanstar@gmail.com>
2015-10-30 12:03:55 +0900

    Insert 버튼 테스트 성공

--- CatHouse_Renewal/CatHouse_Renewal/Controllers/RegistController.cs
+++ CatHouse_Renewal/CatHouse_Renewal/Controllers/RegistController.cs
@@ -42,37 +42,44 @@ namespace CatHouse_Renewal.Controllers
             return View();
         }
 
+
+        [ActionName("Registered")]
+        public ActionResult Registered()
+        {
+            return View();
+        }
+
         [ActionName("MemberCreate")]
         public void MemeberCreate()
         {
             // 비밀번호 암호화
             try
             {
-                string memName = Request.Form["memName"];
-                string memEmail = Request.Form["memEmail"];
-                string memPassword = Request.Form["memPW"];
-                int memPhone = Convert.ToInt32(Request.Form["memPhone"]);
-                string memAddress = Request.Form["memAddress"];
+                //string memName = Request.Form["memName"];
+                //string memEmail = Request.Form["memEmail"];
+                //string memPassword = Request.Form["memPW"];
+                //int memPhone = Convert.ToInt32(Request.Form["memPhone"]);
+                //string memAddress = Request.Form["memAddress"];
 
-                // 실제로 체크
-                bool emailCheck = check.CheckEmail(memEmail);
-                bool passCheck = check.CheckPasswordLength(memPassword);
+                //// 실제로 체크
+                //bool emailCheck = check.CheckEmail(memEmail);
+                //bool passCheck = check.CheckPasswordLength(memPassword);
 
-                // 이메일형식 또는 패스워드 길이가 맞지 않으면
-                if (!emailCheck || !passCheck)
-                {
-                    throw new Exception();
-                }
+                //// 이메일형식 또는 패스워드 길이가 맞지 않으면
+                //if (!emailCheck || !passCheck)
+                //{
+                //    throw new Exception();
+                //}
 
-                // 새로운 멤버 객체 생성
-                MemberModel newMem = new MemberModel();
-                newMem.memName = memName;
-                newMem.memEmail = memEmail;
-                newMem.memPassword = memPassword;
-                newMem.memPhone = memPhone;
-                newMem.memAddress = memAddress;
+                //// 새로운 멤버 객체 생성
+                //MemberModel newMem = new MemberModel();
+                //newMem.memName = memName;
+                //newMem.memEmail = memEmail;
+                //newMem.memPassword = memPassword;
+                //newMem.memPhone = memPhone;
+                //newMem.memAddress = memAddress;
 
-                insertDB.MemberInsert(newMem);
+                //insertDB.MemberInsert(newMem);
                 // 아이템 추가
                 // 결과에 따라서 환영페이지로 이동할지, 오류 메시지 출력 후 홈페이지로 이동할 지 결정
                 //if (queryResult)
@@ -83,11 +90,23 @@ namespace CatHouse_Renewal.Controllers
                 //{
                 //    throw new Exception();
                 //}
+                RedirectToAction("Registered", "Regist");
             }
             catch
             {
                 //return RedirectToAction("Error", "Home");
             }
+        }
+
+        [ActionName("CatCreate")]
+        public void CatCreate()
+        {
+        }
+
+        [ActionName("TraderCreate")]
+        public void TraderCreate()
+        {
+
         }
     }
 }
