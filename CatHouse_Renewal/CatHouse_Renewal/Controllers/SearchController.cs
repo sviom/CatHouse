1a9dad0e62df53fdf808db502a519c55041fc393
HanByulKang <kanghanstar@outlook.com>
2015-11-14 22:24:14 +0900

    li에 클래스 자동 적용 추가

---
+++ CatHouse_Renewal/CatHouse_Renewal/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatHouse_Renewal.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult MapFinder()
+        {
+            return View();
+        }
+
+        public ActionResult FilterFinder()
+        {
+            return View();
+        }
+    }
+}
