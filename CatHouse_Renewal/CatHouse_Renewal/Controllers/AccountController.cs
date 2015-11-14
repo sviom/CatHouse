2506da7e48605e13256d23876cc6ba02f9704651
HanByulKang <kanghanstar@outlook.com>
2015-11-15 00:24:13 +0900

    로그인 관련 기능 추가중

---
+++ CatHouse_Renewal/CatHouse_Renewal/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using CatHouse_Renewal.Models;
+using CatHouse_Renewal.DB;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+
+namespace CatHouse_Renewal.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // 로그인 관련 DB Connection모음
+        LoginConnection loginConn = new LoginConnection();
+        // 점검 관련 함수들 모음
+        Check check = new Check();
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("BeginLogin")]
+        public void BeginLogin(MemberModel memModel)
+        {
+            try
+            {
+                string memEmail = Request.Form["loginMemberId"];
+                string memPassword = Request.Form["loginMemberPassword"];
+
+                DataSet queryResult = loginConn.MemberLogin(memEmail, memPassword);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            // 로그인 확인
+            //if(ModelState.IsValid == true)
+            //{
+            //    int returnValue = 0;
+            //    if(returnValue == 0)
+            //    {
+            //        //로그인 성공
+            //        Session[claGlobal.SessionString_UserID] = memModel.memID;
+            //    }
+            //}
+        }
+    }
+}
---
+++ CatHouse_Renewal/CatHouse_Renewal/DB/LoginConnection.cs
@@ -0,0 +1,56 @@
+﻿using CatHouse_Renewal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CatHouse_Renewal.DB
+{
+    public class LoginConnection
+    {
+        // DBConnection 객체를 이용해 DB를 열고 닫는다.
+        DBConnection db = new DBConnection();
+        // DBConnection 객체에서 받아온 SqlConnection 객체를 받아옴.
+        private static SqlConnection conn;
+
+        public DataSet MemberLogin(string memEmail, string memPassword)
+        {
+            try
+            {
+                // DB를 새로 연다.
+                conn = db.DbOpen();
+                //열려 있으면 사용한다.
+                if (conn.State.ToString() != "Open")
+                {
+                    conn.Open();
+                }
+                // 고유아이디/이름/나이/성별/중성화상태/사진/상태메모
+                string query = "SELECT memEmail, memPassword FROM dbo.Member WHERE memEmail = '" + memEmail + "' AND memPassword = '" + memPassword + "';";
+
+                SqlDataAdapter ddd = new SqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                ddd.Fill(ds, "dbo.Member");
+
+
+                //SqlCommand sqlQuery = new SqlCommand(query, conn);
+                //SqlDataReader item = sqlQuery.ExecuteReader();
+                //while (item.Read())
+                //{
+                //    item["memEmail"].ToString();
+                //    item["memPassword"].ToString();
+                //}
+                //// 실행
+                //int rows = sqlQuery.ExecuteNonQuery();
+                db.DbClose();
+                return ds;
+            }
+            catch (SqlException sqlEx)
+            {
+                sqlEx.Message.ToString();
+                return null;
+            }
+        }
+    }
+}
